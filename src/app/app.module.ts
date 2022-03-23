6339b22752b536a2de5a7fafd119b86497076b0c
Christian Liebel <christian.liebel@thinktecture.com>
2022-03-23 14:26:11 +0100

    Ex #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodosComponent } from './todos/todos.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
