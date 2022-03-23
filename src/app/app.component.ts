ab319edf077a2945a06728531d0eba758f22d548
Christian Liebel <christian.liebel@thinktecture.com>
2022-03-23 15:59:17 +0100

    Ex #7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-pwa';
+
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.versionUpdates.pipe(filter(evt => evt.type === "VERSION_READY")).subscribe(() => {
+      if (confirm('Update verfügbar. Jetzt neu laden?')) {
+        location.reload();
+      }
+    });
+  }
 }
